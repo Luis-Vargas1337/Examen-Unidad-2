abcce4c4abf137efab6f5aa26428818e89b41743
Luis-Vargas1337 <20203tn060@utez.edu.mx>
2021-06-25 16:29:47 -0500

    Examen unidad 2 terminado

---
+++ js/mensaje.js
@@ -0,0 +1,4 @@
+const mensaje = () =>{
+    let busqueda = document.getElementById("busqueda").value;
+    alert(`La busqueda con nombre ${busqueda} no se encuentra por lo cual te solicitamos intentar mas tarde`);
+}
